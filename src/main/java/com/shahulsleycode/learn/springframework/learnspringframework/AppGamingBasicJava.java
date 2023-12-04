84b29cd1cbd22c81e97f93611136e15ba79eab68
Shahul <codewithshahul@outlook.com>
2023-12-04 12:39:13 -0500

    Change Game - Java Interface inaction
    Changing game became easier and simple changes in one class because of Java interface

--- src/main/java/com/shahulsleycode/learn/springframework/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/shahulsleycode/learn/springframework/learnspringframework/AppGamingBasicJava.java
@@ -1,15 +1,17 @@
 package com.shahulsleycode.learn.springframework.learnspringframework;
 
 import com.shahulsleycode.learn.springframework.games.GameRunner;
+import com.shahulsleycode.learn.springframework.games.PacManGame;
 //import com.shahulsleycode.learn.springframework.games.MarioGame;
-import com.shahulsleycode.learn.springframework.games.SuperContraGame;
+//import com.shahulsleycode.learn.springframework.games.SuperContraGame;
 
 public class AppGamingBasicJava {
     
     public static void main(String[] args){
         
         //var game = new MarioGame();
-        var game = new SuperContraGame();
+        //var game = new SuperContraGame();
+        var game = new PacManGame();
         var gameRunner = new GameRunner(game);
 
         gameRunner.run();
